3180db7210327ce17e29a9a8e355507b4fe9d8c9
Gspaiva57004 <gsanatanapaiva123@gmail.com>
2022-09-17 14:22:38 -0300

    java

--- Java/AULAS/src/LojaeCommerce.java
+++ Java/AULAS/src/LojaeCommerce.java
@@ -6,20 +6,31 @@ public class LojaeCommerce {
 	
 	//variaveis
 	
-	String codigo[] = {"GRPO4-01","GRPO4-02","GRPO4-03","GRPO4-04","GRPO4-05","GRPO4-06","GRPO4-07","GRPO4-08","GRPO4-09","GRPO4-10"};
-	String produtos[] = {"Arroz", "Feijão","Macarrão", "Café","Leite","Suco", "Banana","Maça","File de frango", "Papel Higienico"};
-	double valor[] = {19.99, 12.49, 1.99, 18.99 , 16.44, 0.99, 7.99, 4.99, 18.99, 11.99};
-	int estoque[] = {10,10,10,10,10,10,10,10,10,10};
-	
+	String espaco = " | ";
+	String codigo[] = {"GRPO4-01","GRPO4-02","GRPO4-03","GRPO4-04","GRPO4-05","GRPO4-06","GRPO4-07","GRPO4-08","GRPO4-09","GRPO4-10","\t"};
+	String produtos[] = {"Arroz", "Feijão","Macarrão", "Café","Leite","Suco", "Banana","Maça","File de frango", "Papel Higienico","\t"};
+	double valor[] = {19.99, 12.49, 1.99, 18.99 , 16.44, 0.99, 7.99, 4.99, 18.99, 11.99, '\t'};
+	int estoque[] = {10,10,10,10,10,10,10,10,10,10, '\t'};
+	char resposta = 'n';
 	//pergunta
 	
 	System.out.println("Supermercado BARATIX onde o seu dinheiro é valorizado");
-	System.out.println("codigo produto valor estoque");
-		for(int i =0;i<10;i++){
-		
-			System.out.printf("%s %s %.2f %d \n",codigo[i],produtos[i],valor[i],estoque[i] );
-	
+	System.out.print("Deseja fazer a compra? ");
+	resposta = leia.next().charAt(0);
+		if(resposta == 'S' || resposta == 's') {
+			for(int i =0;i<10;i++) {
+				System.out.println("codigo produto valor estoque");
+			System.out.printf(codigo[i] + espaco + produtos[i] + espaco +valor[i] + espaco + estoque[i] + "\n");
+			System.out.print("-----------------------------------\n");
+				
+			}
+		}else {
+			System.out.println("Até breve.");
 		}
+			
+	
+	
+		
 		
 	
 	}
